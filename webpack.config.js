e376da3f6c1c7333235029e463049cacebe082f8
YM <a834319313@163.con>
2021-09-03 18:00:11 +0800

    提取CSS文件之前的更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
 
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "我怕了",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
